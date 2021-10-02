12b811169c2dc86a7630c9f56db752d02acf7304
ol-bashkin <ol.bashkin@gmail.com>
2021-10-02 18:41:22 +0300

    Vite init

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Test Weather Archive</h1>
+  <a href="https://github.com/ol-bashkin/test-weather-archive#readme" target="_blank">Documentation</a>
+`;
